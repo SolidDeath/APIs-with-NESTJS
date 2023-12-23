0a575bb18d9bdc2f808cc7371b89685b8e5b4905
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-12-23 15:17:18 +0200

    Add Prisma module and service

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global() //makes the PrismaService available everywhere in the app
+@Module({
+    providers: [PrismaService],
+    exports: [PrismaService],
+})
+export class PrismaModule {}
