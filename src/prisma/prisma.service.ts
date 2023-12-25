2723a6f7dcde5d201e20cf9b4139e551ef3adba2
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-12-25 15:58:10 +0200

    Add @nestjs/config dependency and configure global configuration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
+        //calls the constructor of the parent class
         super({
             datasources: {
                 db: {
-                    url: process.env.DATABASE_URL,
+                    url: config.get(
+                        'DATABASE_URL',
+                    ),
                 },
             },
-        }); //calls the constructor of the parent class
+        });
     }
 }
