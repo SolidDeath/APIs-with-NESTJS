64bc69708e73bf4265fafdf2ff7650092651edc6
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-12-25 14:27:49 +0200

    Refactor auth.service.ts to handle duplicate credentials error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+    ForbiddenException,
+    Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -12,20 +16,35 @@ export class AuthService {
         const hash = await argon.hash(
             dto.password,
         );
+        try {
+            // Save ths new user in the db
+            const user =
+                await this.prisma.user.create({
+                    data: {
+                        email: dto.email,
+                        hash,
+                    },
+                });
 
-        // Save ths new user in the db
-        const user =
-            await this.prisma.user.create({
-                data: {
-                    email: dto.email,
-                    hash,
-                },
-            });
+            delete user.hash; //temp solution
 
-        delete user.hash; //temp solution
-
-        // return the saved user
-        return user;
+            // return the saved user
+            return user;
+        } catch (error) {
+            if (
+                error instanceof
+                PrismaClientKnownRequestError
+            ) {
+                // if is prisma error
+                if (error.code === 'P2002') {
+                    //Error code defined in prisma docs - duplicate field
+                    throw new ForbiddenException(
+                        'Credentials are already in use',
+                    );
+                }
+            }
+            throw error;
+        }
     }
 
     signin() {
