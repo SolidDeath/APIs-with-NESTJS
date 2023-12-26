4d882cf313fb614442e9ab1453d74146cd26ec32
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-12-26 23:20:59 +0200

    Add JWT guard and strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+    ExtractJwt,
+    Strategy,
+} from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+    Strategy,
+    'jwt',
+) {
+    constructor(
+        config: ConfigService,
+        private prisma: PrismaService, // to get information about the user directly from the database
+    ) {
+        super({
+            jwtFromRequest:
+                ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('JWT_SECRET'),
+        });
+    }
+
+    // The validate() method is called by Passport after it has decoded the JWT.
+    async validate(payload: {
+        // the payload is the decoded JWT tok.endsWith(searchString, endPosition)
+        sub: number;
+        email: string;
+    }) {
+        const user =
+            await this.prisma.user.findUnique({
+                where: {
+                    id: payload.sub,
+                },
+            });
+
+        delete user.hash; // remove the hash from the user object
+
+        return user;
+    }
+}
