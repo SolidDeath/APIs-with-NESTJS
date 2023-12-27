c5aae8251f5c904affe92a4152b6a218c4496cdb
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-12-27 15:23:01 +0200

    Update ESLint and TypeScript configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,28 @@
 export default {
-    extends: ['recommended'],
-    rules: {
-        'no-unused-vars': 'on',
-    },
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 6,
+        project: 'tsconfig.json',
+        sourceType: 'module',
     },
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    root: true,
     env: {
         node: true,
-        browser: true,
-        es6: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix':
+            'off',
+        '@typescript-eslint/explicit-function-return-type':
+            'off',
+        '@typescript-eslint/explicit-module-boundary-types':
+            'off',
+        '@typescript-eslint/no-explicit-any':
+            'off',
     },
 };
